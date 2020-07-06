742194e62a2fd34eab7dacb698f4bbe0eb5cf443
david <ckhoct@gmail.com>
2020-07-06 18:00:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Description
+
+Demo 微型 CMS 效果
+
+套用 CC-by 板型結合 GAS 與表單
---
+++ app.js
@@ -0,0 +1,49 @@
+var ifDebug = false;
+
+window.onload = function () {
+  var websiteTitle = document.getElementById("websiteTitle");
+  var websiteIntro = document.getElementById("websiteIntro");
+  var headerTitle = document.getElementById("headerTitle");
+  var headerIntro = document.getElementById("headerIntro");
+  var headerBtn = document.getElementById("headerBtn");
+  var footerTitle = document.getElementById("footerTitle");
+  var footerIntro = document.getElementById("footerIntro");
+  var footerBtn = document.getElementById("footerBtn");
+  var mainItems = document.querySelector(".main.items");
+
+  var baseURL =
+    "https://script.google.com/macros/s/AKfycbzeyPtkYs2K-P8cTuHROtLqyUNeLE8on4NOi7xooMurG5J8f3I/exec";
+
+  fetch(ifDebug || baseURL)
+    .then((res) => res.json())
+    .then((json) => {
+      const { data } = json;
+      const mainBlocks = data.slice(2, 6);
+      mainItems.innerHTML = mainBlocks.map(articleItem).join("");
+
+      websiteTitle.innerText = data[0].title;
+      websiteIntro.innerText = data[0].intro;
+      headerTitle.innerText = data[1].title;
+      headerIntro.innerText = data[1].intro;
+      headerBtn.innerText = data[1].btn;
+      headerBtn.href = data[1].link;
+      footerTitle.innerText = data[6].title;
+      footerIntro.innerText = data[6].intro;
+      footerBtn.innerText = data[6].btn;
+      footerBtn.href = data[6].link;
+    }).catch(err => alert('網路不穩定'));
+};
+
+const articleItem = (item, id) => {
+  const { title, intro, btn, link } = item;
+  return `<article class="item">
+    <header>
+        <a href="#"><img width="100%" src="images/pic0${id + 1}.jpg" alt="" /></a>
+        <h3>${title}</h3>
+    </header>
+    <p>${intro}</p>
+    <ul class="actions">
+        <li><a href="${link}" class="button">${btn}</a></li>
+    </ul>
+</article>`;
+};
